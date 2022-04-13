e2b50fcaf141c17fc4b0d9e7ab4494fdde2fbf80
James Blacklock <jamesblacklock@protonmail.com>
2022-04-13 13:08:47 -0400

    committing to "lecture"

--- api/hubs/hubs-router.js
+++ api/hubs/hubs-router.js
@@ -5,18 +5,6 @@ const Messages = require('../messages/messages-model.js');
 
 const router = express.Router();
 
-function validateHub(req, res, next) {
-  // in httpie, send numbers with key:=value
-  if(typeof req.body.name != 'string' || req.body.name.trim() == '') {
-    res.status(400).json({ message: 'name is required' });
-    return;
-  }
-
-  req.hub = { name: req.body.name.trim() };
-  next();
-}
-
-
 
 function moodyGatekeeper(req, res, next) {
   let r = Math.floor(Math.random() * 3);
