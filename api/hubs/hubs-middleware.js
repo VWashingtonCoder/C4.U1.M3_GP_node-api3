d182d79c943bcf99d9aa0bab5484bf8ad13c2b16
James Blacklock <jamesblacklock@protonmail.com>
2022-04-13 13:11:05 -0400

    committing to "lecture"

--- api/hubs/hubs-middleware.js
+++ api/hubs/hubs-middleware.js
@@ -1,15 +1,18 @@
 function validateHub(req, res, next) {
-    // in httpie, send numbers with key:=value
-    if(typeof req.body.name != 'string' || req.body.name.trim() == '') {
-      res.status(400).json({ message: 'name is required' });
-      return;
-    }
-  
-    req.hub = { name: req.body.name.trim() };
-    next();
-  }
+	// in httpie, send numbers with key:=value
+	if (typeof req.body.name != 'string' || req.body.name.trim() == '') {
+		res.status(400).json({ message: 'name is required' });
+		return;
+	}
+
+	req.hub = { name: req.body.name.trim() };
+	next();
+}
 
 
-  module.exports = {
-    validateHub,
-  }
+
+
+
+module.exports = {
+	validateHub,
+};
