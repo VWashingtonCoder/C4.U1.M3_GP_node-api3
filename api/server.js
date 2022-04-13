c47324c6c172004a9d65ad788d90f7ac412fc757
James Blacklock <jamesblacklock@protonmail.com>
2022-04-13 12:15:02 -0400

    committing to "lecture"

--- api/server.js
+++ api/server.js
@@ -1,3 +1,10 @@
+// 1. express receives a request
+// 2. looks through its middleware for a match
+// 3. hands off control to that middleware
+// 4. middleware may to 1 of 2 things:
+//     1. send a response back
+//     2. pass on the control to the next middleware
+
 const express = require('express'); // importing a CommonJS module
 
 const hubsRouter = require('./hubs/hubs-router.js');
@@ -8,6 +15,7 @@ server.use(express.json());
 
 server.use('/api/hubs', hubsRouter);
 
+// server.use('GET', '/', (req, res) => {})
 server.get('/', (req, res) => {
   res.send(`
     <h2>Hubs API</h2>
